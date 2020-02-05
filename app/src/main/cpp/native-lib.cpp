90f0016f847a4f5e11ea88e303aeb2cc932bf5c6
yinjimmy <jimmy.yin5@gmail.com>
2020-02-05 21:29:18 +0700

    add admob

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,51 @@
 #include <jni.h>
 #include <string>
 
+#include "sdkbox/Sdkbox.h"
+#include "PluginAdMob/PluginAdMob.h"
+
+namespace cocos2d {
+    const char* cocos2dVersion()
+    {
+        return "cocos2d-x-3.17";
+    }
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_sdkbox_sdkboxcpp_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
+
+    auto str = R"(
+                    {
+                        "android": {
+                            "AdMob": {
+                                "testdevice": "FE20924C46522E2E204587EB339897C6,kGADSimulatorID",
+                                "test": false,
+                                "ads": {
+                                    "rewarded": {
+                                        "type": "rewarded_video",
+                                        "id": "ca-app-pub-1329374026572143/8251968111"
+                                    }
+                                },
+                                "appid": "ca-app-pub-1329374026572143~7139010117"
+                            }
+                        },
+                        "ios": {
+                            "AdMob": {
+                                "testdevice": "kGADSimulatorID",
+                                "test": false,
+                                "safearea": true,
+                                "ads": {
+                                },
+                                "appid": "ca-app-pub-1329374026572143~2708810518"
+                            }
+                        }
+                    }
+                 )";
+    sdkbox::setConfig(str);
+    sdkbox::PluginAdMob::init();
+
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/sdkbox/sdkboxcpp/MainActivity.java
+++ app/src/main/java/com/sdkbox/sdkboxcpp/MainActivity.java
@@ -2,8 +2,11 @@ package com.sdkbox.sdkboxcpp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.sdkbox.plugin.SDKBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +20,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SDKBox.init(this);
+
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
     }
+
+    // sdkbox start
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SDKBox.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SDKBox.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SDKBox.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SDKBox.onPause();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!SDKBox.onBackPressed()) {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(!SDKBox.onActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    // sdkbox end
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
